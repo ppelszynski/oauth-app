0013ee39e17306111b6ced791336563af466b1af
Przemysław Pełszyński <przemek@MacBook-Pro-Przemysaw.local>
2023-05-28 22:16:51 +0200

    Added oauth app

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "home#index"
+end
